e2bd0b4d0d1effd722ccccb09e338651059c1c7d
garvsl <garvsehgal7@gmail.com>
2023-12-27 16:39:42 -0500

    resolve another vercel issue

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,9 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import '@/styles/base.css';
-import { useAuth } from '@/components/authProvider';
 import { Inter } from 'next/font/google';
-import routes from 'next-routes';
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
 import { Provider } from "@/components/provider";
-import { supabase } from "@/lib/initSupabase";
 import { AuthProvider } from '@/components/authProvider';
-import { handleAuthChange, AuthTokens, checkTokens } from './connections/authUtils';  //import the two things we will be using 
-import { type AppProps } from 'next/app'
-import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs'
-import { SessionContextProvider, Session } from '@supabase/auth-helpers-react';
 import ProtectedRoute from "@/components/protectedRoute"
-import { useState } from 'react'
 const inter = Inter({
   variable: '--font-inter',
   subsets: ['latin'],
