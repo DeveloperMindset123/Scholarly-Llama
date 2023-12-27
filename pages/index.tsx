7bd604e3e6a91ad5fdcf14d22e22d0923037d70b
garvsl <garvsehgal7@gmail.com>
2023-12-27 12:18:48 -0500

    tiny mistake with numbers

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <section className="flex flex-col lg:flex-row">
     
-      <IntroSection number={"02"} text={"ScholarlyLlama -  Our friend right here transforms your textbooks into dynamic chatbots, offering a unique and engaging way to explore and understand your course materials."}/>
+      <IntroSection number={"01"} text={"ScholarlyLlama -  Our friend right here transforms your textbooks into dynamic chatbots, offering a unique and engaging way to explore and understand your course materials."}/>
 
       <section className="hidden lg:flex h-screen w-full flex-col justify-center items-center bg-[#d6ebe9] p-9">
         <Image src={robot} alt="Man sitting in wheelchair" />
