70266718e5d4d643018f5387bc4832aa29d7ab9f
garvsl <garvsehgal7@gmail.com>
2023-12-28 16:25:55 -0500

    incorrect redirect

--- components/ui/dashboardButton.tsx
+++ components/ui/dashboardButton.tsx
@@ -17,7 +17,7 @@ export default function DashboardButton() {
           await supabase.auth.signInWithOAuth({
             provider: 'google',
             options: {
-              redirectTo: 'http://localhost:3000/dashboard',
+              redirectTo: 'https://scholarly-llama.vercel.app//dashboard',
               queryParams: {
                 prompt: 'consent',
               },
